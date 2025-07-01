da90ce611afde3496df62f4005a371364c749dfb
KatrionaGoldmann <katrionagoldmann@gmail.com>
2025-07-01 09:33:48 +0100

    update api_utils tests

--- src/amber_inferences/utils/api_utils.py
+++ src/amber_inferences/utils/api_utils.py
@@ -1,6 +1,5 @@
 import requests
 import sys
-import boto3
 
 
 def get_buckets(s3_client):
@@ -68,67 +67,3 @@ def count_files(s3_client, bucket_name, prefix):
         "other_count": other_count,
         "other_file_types": other_file_types,
     }
-
-
-# TODO archive this function properly
-def deployments_summary_archive(
-    aws_credentials,
-    include_inactive=False,
-    subset_countries=None,
-    subset_deployments=None,
-    summary_subdir="snapshot_images",
-    include_image_count=True,
-):
-    """
-    Print information about deployments from the API.
-    This function is covered in deployment_summary
-    """
-
-    # Setup boto3 session
-    session = boto3.Session(
-        aws_access_key_id=aws_credentials["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=aws_credentials["AWS_SECRET_ACCESS_KEY"],
-        region_name=aws_credentials["AWS_REGION"],
-    )
-    s3_client = session.client("s3", endpoint_url=aws_credentials["AWS_URL_ENDPOINT"])
-
-    # Get deployments
-    username = aws_credentials["UKCEH_username"]
-    password = aws_credentials["UKCEH_password"]
-    all_deployments = get_deployments(username, password)
-
-    if not include_inactive:
-        all_deployments = [x for x in all_deployments if x["status"] == "active"]
-
-    all_countries = list(set([dep["country"].title() for dep in all_deployments]))
-
-    if subset_countries:
-        subset_countries = [x.title() for x in subset_countries]
-        all_countries = [x for x in all_countries if x in subset_countries]
-
-    deployment_summary = {}
-
-    for country in all_countries:
-        country_depl = [x for x in all_deployments if x["country"].title() == country]
-
-        if not country_depl:
-            print(f"No deployments found for country: {country}")
-            continue
-
-        if subset_deployments:
-            country_depl = [
-                x for x in country_depl if x["deployment_id"] in subset_deployments
-            ]
-
-        for dep_info in country_depl:
-            prefix = f"{dep_info['deployment_id']}/{summary_subdir}/"
-            bucket_name = dep_info["country_code"].lower()
-
-            if include_image_count:
-                print(f'Counting files in {dep_info["deployment_id"]}...')
-                counts = count_files(s3_client, bucket_name, prefix)
-                dep_info["file_types"] = counts
-
-            deployment_summary[dep_info["deployment_id"]] = dep_info
-
-    return deployment_summary
