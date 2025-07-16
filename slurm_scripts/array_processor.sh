221e1b25fa9c91878a5c2bc618b97f71aca21d85
Dylan Carbone <dylcar@login-02.jasmin.ac.uk>
2025-07-16 13:39:01 +0100

    change to python path

--- slurm_scripts/array_processor.sh
+++ slurm_scripts/array_processor.sh
@@ -7,14 +7,13 @@ echo "Chunk ID: ${SLURM_ARRAY_TASK_ID}"
 
 source ~/miniforge3/bin/activate
 conda activate "~/amber/"
-export PYTHONPATH="/home/users/dylcar/AMI_classifications/src"
+export PYTHONPATH="/home/users/dylcar/amber-inferences/src"
 
 IFS=' ' read -r -a session_names_array <<< "$session_names_string"
 this_session="${session_names_array[$SLURM_ARRAY_TASK_ID - 1]}"
 echo "Session date: ${this_session}"
 
 echo "Output csv: ${output_base_dir}/${deployment_id}/${deployment_id}_${this_session}.csv"
-
 
 # Set up GPU monitoring log
 GPU_LOG_DIR="${output_base_dir}/${deployment_id}/compute_resources"
